5ba062338509a2794841db957e8abfc2eff92768
HiddenMotives <HiddenMotives@users.noreply.github.com>
2017-07-19 13:18:18 -0600

    1.0.6 VPNGuard Update

--- src/basprohop/VPNGuard.php
+++ src/basprohop/VPNGuard.php
@@ -5,10 +5,11 @@ use basprohop\libraries\Async;
 use basprohop\libraries\SimpleCache;
 use basprohop\libraries\CommandFunctions;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
 use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
 
@@ -16,15 +17,17 @@ use pocketmine\utils\Config;
 class VPNGuard extends PluginBase implements Listener {
 
     private $commands;
-    public $cache, $cfg, $subnet_list, $cfgCommands = array(), $subnets = array();
+    public $cache, $cfg, $subnet_list, $country_list, $cfgCommands = array(), $subnets = array(), $countries = array();
 
     public function onEnable() {
         @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
         $this->cfg = $this->getConfig()->getAll();
         $this->subnet_list = new Config($this->getDataFolder() . "banned.yml", Config::YAML);
+        $this->country_list = new Config($this->getDataFolder() . "countries.yml", Config::YAML);
 
         $this->subnets = $this->subnet_list->get("subnets", []);
+        $this->countries = $this->country_list->get("country_codes", []);
 
         $configCommands = $this->cfg["command"];
         foreach ($configCommands as $configCommand) {
@@ -47,16 +50,26 @@ class VPNGuard extends PluginBase implements Listener {
             $this->getLogger()->info(TextFormat::YELLOW . "No API key specified, using free version.");
         }
 
+        if(!((strtolower($this->cfg["country_mode"]) === "blacklist") || (strtolower($this->cfg["country_mode"]) === "whitelist"))) {
+          $this->getLogger()->critical("The country_mode setting in config.yml must either be 'blacklist' or 'whitelist'.");
+          $this->getServer()->shutdown();
+        }
+
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-    public function onPlayerLogin(PlayerPreLoginEvent $event) {
+    /**
+  	 * @param PlayerJoinEvent $event
+  	 *
+  	 * @priority HIGHEST
+  	 */
+    public function onPlayerLogin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
         $ipAddress = $player->getAddress();
 
         if ($this->cfg["logging"]) {
             $this->getLogger()->info(TextFormat::WHITE . "Player " . TextFormat::GOLD . $player->getName() .
-                TextFormat::WHITE . " is trying to connect with IP: " . TextFormat::GRAY . $ipAddress);
+                TextFormat::WHITE . " has connected with the IP: " . TextFormat::GRAY . $ipAddress);
         }
 
         $iplong = ip2long($ipAddress);
@@ -65,23 +78,19 @@ class VPNGuard extends PluginBase implements Listener {
 
             if ( ($range != null) && (count($range) == 2)) {
                 if (($iplong <= (ip2long($range[1]))) && ((ip2long($range[0])) <= $iplong)) {
-                    $player->close("", $this->cfg["ban-message"]);
+                    $player->close("",$this->cfg["ban-message"]);
 
                     if($this->cfg["logging"]) {
                         $this->getLogger()->info(TextFormat::DARK_RED . $player->getName() . TextFormat::WHITE .
                             " has been disconnected for being in the subnet " . $subnet . " which is banned.");
                     }
-
-                    $event->setCancelled(true);
                 }
             }
         }
-        $this->getServer()->getScheduler()->scheduleAsyncTask(
-            new Async(1, $player->getName(), $ipAddress, $this->getUserAgent(), $this->cfg, $this->cfgCommands, $this->cache));
+        $this->getServer()->getScheduler()->scheduleAsyncTask(new Async(1, $this, $player->getName(), $ipAddress));
     }
 
     /**
-     * Used for internal networking purposes.
      * Function that makes a useragent based on Plugin version and PocketMine version.
      * @return string - User agent.
      */
@@ -118,7 +127,7 @@ class VPNGuard extends PluginBase implements Listener {
     }
 
     //********************************* Commands ********************************//
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
         $numArgs = count($args);
         $insufficientPerm = $this->msg(TextFormat::RED . "You do not have permission to use this command!");
         switch ($command->getName()) {
@@ -151,31 +160,30 @@ class VPNGuard extends PluginBase implements Listener {
                                 if (($numArgs == 2)) {
                                     $this->commands->cmdLookup($sender, $args[1]);
                                 } else {
-                                    $sender->sendMessage($this->msg("Usage: /vpnguard lookup <ipv4 address>"));
+                                    $sender->sendMessage($this->msg("Usage: /vpnguard lookup <ipv4 address/player>"));
                                 }
                             } else {
                                 $sender->sendMessage($insufficientPerm);
                             }
                             return true;
 
-                        } else if (strtolower($args[0]) === "ban") {
-                            if ($sender->hasPermission("vpnguard.command.ban")) {
-                                if (($numArgs == 2)) {
-                                    $this->commands->cmdBan($sender, $args[1]);
+                        } else if (strtolower($args[0]) === "subnet") {
+                            if ($sender->hasPermission("vpnguard.command.subnet")) {
+                                if (($numArgs == 3)) {
+                                    $this->commands->cmdSubnet($sender, $args[1], $args[2]);
                                 } else {
-                                    $sender->sendMessage($this->msg("Usage: /vpnguard ban <ipv4 address/subnet>"));
+                                    $sender->sendMessage($this->msg("Usage: /vpnguard subnet <ban/unban> <ipv4 address/subnet>"));
                                 }
                             } else {
                                 $sender->sendMessage($insufficientPerm);
                             }
                             return true;
-
-                        } else if (strtolower($args[0]) === "unban") {
-                            if ($sender->hasPermission("vpnguard.command.unban")) {
-                                if (($numArgs == 2)) {
-                                    $this->commands->cmdUnban($sender, $args[1]);
+                        } else if (strtolower($args[0]) === "country") {
+                            if ($sender->hasPermission("vpnguard.command.country")) {
+                                if (($numArgs == 3)) {
+                                    $this->commands->cmdCountry($sender, $args[1], $args[2]);
                                 } else {
-                                    $sender->sendMessage($this->msg("Usage: /vpnguard unban <ipv4 address/subnet>"));
+                                    $sender->sendMessage($this->msg("Usage: /vpnguard country <add/remove> <country code>"));
                                 }
                             } else {
                                 $sender->sendMessage($insufficientPerm);
